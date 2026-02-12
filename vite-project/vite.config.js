d93574d3226ef12d2e85f7439ec84833c1debe20
Your Name <your-email@example.com>
2026-02-12 08:56:18 +0000

    tailwind config

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import tailwind form '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    tailwindc()
   ],
 })
