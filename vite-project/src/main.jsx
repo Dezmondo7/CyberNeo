b7139e0894a76e8c26682950032cbc2383008e56
Your Name <your-email@example.com>
2026-02-12 08:45:24 +0000

    renaming files

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import app from './app.jsx'
 import './app.css'
 
 createRoot(document.getElementById('root')).render(
